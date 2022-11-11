8611441a8d084f20ddc0ff1369dcc4d273814166
Loïc <59501884+LoicE5@users.noreply.github.com>
2022-11-11 14:15:36 +0100

    Working version for jar as well as IDE files access, using 2 different protocols

--- src/tools/Utils.java
+++ src/tools/Utils.java
@@ -1,9 +1,12 @@
 package tools;
 
-import org.apache.commons.io.IOUtils;
-
-import java.io.FileInputStream;
-import java.nio.charset.StandardCharsets;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Utils {
     public static String getLastSubstring(String input, int index){
@@ -11,15 +14,26 @@ public class Utils {
     }
     public static String getFileAsString(String path){
         try {
-            FileInputStream file;
-            file = new FileInputStream(path);
-            String output = IOUtils.toString(file, StandardCharsets.UTF_8);
-            file.close();
-            return output;
+            if(isRunningFromJar()){
+                String absoluteRoot = URLDecoder.decode((new File(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResource(".")).getPath())).getAbsolutePath());
+                Scanner scan= new Scanner(new FileReader(path.replace("./",absoluteRoot+"/")));
+                String output = "";
+                while(scan.hasNext()){
+                    output += scan.next()+" ";
+                }
+                return output;
+            } else {
+                return Files.readString(Path.of(path));
+            }
         } catch (Exception e){
-            System.out.println(e);
+            System.out.println("Error in getFileAsString. Running from Jar : "+isRunningFromJar()+". "+e);
             return "";
         }
+    }
+
+    public static boolean isRunningFromJar(){
+        String protocol = Objects.requireNonNull(Utils.class.getResource("Utils.class")).getProtocol();
+        return (protocol.equals("jar") || protocol.equals("rsrc")); // jar is the default protocol when running for jar file, rsrc is used by Eclipse-exported jar files, file is used otherwise (from IDE)
     }
 
     public static String anonymizeCardNumber(String cardNumber){
